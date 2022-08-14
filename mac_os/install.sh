cb05d12144a03c01dbf8b93c83e9a5a1fa109c65
iroha <git@iroha-s.jp>
2022-08-14 17:58:23 +0900

    mac setup

---
+++ mac_os/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# --- Finder ---
+# ステータスバーを表示
+defaults write com.apple.finder ShowStatusBar -bool true
+# パスバーを表示
+defaults write com.apple.finder ShowPathbar -bool true
+killall Finder
+
+# ネットワークストレージに .DS_Store ファイルを作成しない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# USBメモリに .DS_Store ファイルを作成しない
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# --- Security ---
+# ファイアーウォールをオン
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+
+# --- SystemUIServer関係 ---
+# 時計で日付を表示（例：9月20日(木) 23:00）
+defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d HH:mm:ss'
+# バッテリーの割合（%）を表示
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+# スクリーンショットのドロップシャドウを付けない
+defaults write com.apple.screencapture disable-shadow -bool true
+killall SystemUIServer
+
+# ---　TextEdit　---
+# リッチテキストから標準テキストに変更
+defaults write com.apple.TextEdit RichText -int 0
+
+echo "👍 MacOS setting is done!"
