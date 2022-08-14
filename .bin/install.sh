71578acaba094e1077b78efb587ef3b4eac9b0bc
iroha <git@iroha-s.jp>
2022-08-14 17:13:35 +0900

    シンボリックリンク

--- .bin/install.sh
+++ .bin/install.sh
@@ -8,21 +8,21 @@ helpmsg() {
 
 link_to_homedir() {
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
+  if [ ! -d "$HOME/.dotbackup" ]; then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
   local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
+  if [[ "$HOME" != "$dotdir" ]]; then
     for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
+      [[ $(basename $f) == ".git" ]] && continue
+      if [[ -L "$HOME/$(basename $f)" ]]; then
+        command rm -f "$HOME/$(basename $f)"
       fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+      if [[ -e "$HOME/$(basename $f)" ]]; then
+        command mv "$HOME/$(basename $f)" "$HOME/.dotbackup"
       fi
       command ln -snf $f $HOME
     done
@@ -31,21 +31,21 @@ link_to_homedir() {
   fi
 }
 
-while [ $# -gt 0 ];do
+while [ $# -gt 0 ]; do
   case ${1} in
-    --debug|-d)
-      set -uex
-      ;;
-    --help|-h)
-      helpmsg
-      exit 1
-      ;;
-    *)
-      ;;
+  --debug | -d)
+    set -uex
+    ;;
+  --help | -h)
+    helpmsg
+    exit 1
+    ;;
+  *) ;;
+
   esac
   shift
 done
 
 link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
+ln -s ~/dotfiles/.config/git/ ~/.config
 command echo -e "\e[1;36m Install completed!!!! \e[m"
